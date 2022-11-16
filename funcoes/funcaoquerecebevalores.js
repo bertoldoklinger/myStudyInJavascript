fdece0d5889a6e040d382d0d182e14185733d86e
Bertoldo Klinger <bertoldokbrj@gmail.com>
2022-11-16 16:07:18 -0300

    funcoesquerecebemvaloresaula

---
+++ funcoes/funcaoquerecebevalores.js
@@ -0,0 +1,9 @@
+//pedir para o usuario digitar 2 numeros que representem os lados de um retangulo
+//mostrar na tela a area total do retangulo
+let base = parseFloat(prompt("Digite a base"));
+let altura = parseFloat(prompt("Digite a altura"));
+function areaRetangulo(b, a) {
+  let areaRetangulo = (b * a);
+  alert("A área total é: " + areaRetangulo);
+}
+
