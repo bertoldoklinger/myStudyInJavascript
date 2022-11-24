581cac7a8f9e04e4418accdf1eabfcdc30913f9a
bertoldoklinger <bertoldokbrj@gmail.com>
2022-11-24 20:56:31 -0300

    added loop and arrays exercises

---
+++ exercises/loopsarraysexercicios.js
@@ -0,0 +1,72 @@
+// Crie um loop while que percorre por todos
+// os itens da array enquanto a variavel 'i'
+// for menor que o tamanho da array.
+
+// Imprima os itens no console
+
+let redesSociais = ['Facebook', 'Twitter', 'Instagram', 'Dev community', 'Linkedin', 'reddit', 'WhatsApp', 'Snapchat', 'Tik tok'];
+
+for (let i = 0; i < redesSociais.length; i++){
+console.log(redesSociais[i])
+}
+
+
+// Crie um loop for para imprimir no console
+// números de 0 a 10
+
+for(let i = 0; i>=0 && i<=10; i++){
+console.log(i)
+}
+
+// Crie um loop for para imprimir no console
+// números de 0 a 10
+
+for (let i = 0; i <= 10; i++){
+  console.log(i);
+}
+
+// Agora utilize um loop for para imprimir no console
+// os número de 10 a 0
+
+for (let i = 10; i > 0; i--){
+  console.log(i);
+}
+
+
+// Loop do... while
+// Escreva um loop do... while para imprimir no console
+// os número entre 0 e 20
+
+let j = 0;
+do {
+  console.log(j);
+  
+  j++;
+} while (j <= 20);
+
+
+
+// Crie um loop do... while que percorre por todos
+// os itens da array enquanto a variavel 'i'
+// for menor que o tamanho da array.
+
+// Imprima os itens no console
+
+let cores = ['Vermelho', 'Amarelo', 'Azul', 'Verde', 'Preto', 'Branco'];
+
+let i = 0;
+do {
+  console.log(cores[i]);
+  i++;
+} while (i < cores.length);
+
+
+// Crie um loop for e imprima apenas os números
+// pares de 0 a 20
+
+for (let i = 0; i <= 20; i++){
+  if(i % 2 === 0){
+    console.log(i);
+  }
+}
+
