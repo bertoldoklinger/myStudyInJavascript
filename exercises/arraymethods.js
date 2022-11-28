1aa495fe4699de667bfe0f38256f7dce9dfd78a4
Bertoldo Klinger <bertoldokbrj@gmail.com>
2022-11-28 10:12:11 -0300

    added new array methods exercises

--- exercises/arraymethods.js
+++ exercises/arraymethods.js
@@ -54,3 +54,101 @@ const check = (a,x)=>a.includes(x)
     return false
   }
 */
+
+/*
+    1. Create a variable called "multiple" and initialize it to 5
+    2. Create an array with the numbers 10-15 (inclusive)
+    3. Loop through the array and on each iteration:
+        - Multiply the number by the multiple and print out the result
+        - As you're printing, use this format (example):
+            10 x 5 = 50
+            11 x 5 = 55
+            ...
+            15 x 5 = 75
+    BONUS: Try to see if you can make it so that all you need to change
+    is the "multiple" variable and the program still works correctly.
+*/
+
+let multiple = 5;
+
+let newArr = [10, 11, 12, 13, 14, 15];
+
+let len = newArr.length;
+
+for (let i = 0; i < len; i++) {
+  let result = newArr[i] * multiple;
+
+  console.log(`${newArr[i]} x ${multiple} = ${result}`);
+}
+
+/*
+    Create a variable called "greeting" and initialize it to:
+        "Hello, nice to meet you!"
+    
+    Use a loop to loop through this String (just like you would an array)
+        - On each loop iteration, print out what is at that index
+    WHY does this happen?
+    WHAT is a String, really?
+*/
+
+let greeting = "Hello, nice to meet you!";
+
+let leng = greeting.length;
+
+for (let i = 0; i < leng; i++) {
+  console.log(greeting[i]);
+}
+
+/*
+    Create a variable called "total" that starts at 0
+    Create an array called "grades" with the following values:
+        - 55
+        - 63
+        - 82
+        - 98
+        - 91
+        - 43
+        
+    Figure out how to print out the AVERAGE grade
+    (This is the sum of all grades divided by the number of grades)
+*/
+
+let total = 0;
+
+const grades = [55, 63, 82, 98, 91, 43];
+
+for (let i = 0; i < grades.length; i++) {
+  total += grades[i];
+}
+console.log(total / grades.length);
+
+/*
+    Create a variable called "bakery" that points to an array
+    Fill the array with the following String:
+        - "Cake"
+        - "Cookie"
+        - "Bread"
+        - "Scone"
+    Print out bakery to make sure it has these 4 Strings in it.
+    Create another variable called "myBakery" and assign it
+    to "bakery" that we declared previously
+    Add the following items to "myBakery":
+        - "Croissant"
+        - "Granola"
+    Print out myBakery and bakery and observe the contents.
+    WHY is this?
+*/
+
+let bakery = [];
+bakery.push("Cake");
+bakery.push("Cookie");
+bakery.push("Bread");
+bakery.push("Scone");
+console.log(bakery);
+
+let myBakery = bakery;
+
+myBakery.push("Croissant");
+myBakery.push("Granola");
+
+console.log(myBakery);
