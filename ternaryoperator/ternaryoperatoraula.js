1481d6ae4694d2fd119c1fe883a61035204c71ae
Bertoldo Klinger <bertoldokbrj@gmail.com>
2022-12-27 16:27:14 -0300

    added aula operador ternário

---
+++ ternaryoperator/ternaryoperatoraula.js
@@ -0,0 +1,42 @@
+/*
+const person = {
+  name: 'bertoldo',
+  age:23,
+  haveLicense:true,
+}
+
+person.age >= 18 && person.haveLicense === true ? console.log('Você pode dirigir legalmente!') : console.log('Você não pode dirigir!');
+
+*/
+/*
+const happyCheck = (happy) => {
+  if(happy === true){
+    return 'Yay!';
+  }else {
+    return 'Nope';
+  }
+}
+
+const happyCheck2 = (happy) => {
+  return happy === true ? 'Yay!' : 'Nope!'
+}
+
+console.log(happyCheck2(true))
+
+const happyCheck3 = (happy) => (happy === true ? 'Yay' : 'Nope')
+
+console.log(happyCheck3(false))
+*/
+/*
+const nums = [5, 10, 15, 20, 25, 30]
+
+const mappedNums = nums.map((num)=>{
+  return  num > 15
+  ? num * 2 
+  : num * 5
+})
+console.log(mappedNums)
+
+const overUnder = nums.map((num)=> (num > 10 ? 'Over' : 'Under'))
+console.log(overUnder)
+*/
