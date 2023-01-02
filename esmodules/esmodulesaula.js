a63898ed6481cdc35b048229ba23d736cd5d88fd
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-01-02 15:16:40 -0300

    added es modules class

---
+++ esmodules/esmodulesaula.js
@@ -0,0 +1,18 @@
+const person = {
+  name: 'Bertoldo',
+  age: 23,
+  hobbies : ['nba2k', 'fifa23', 'csgo']
+};
+
+const printHobbies = () => {
+  console.log(person.hobbies)
+}
+
+const car = {
+  model: 'Chevrolet',
+  year: 2009,
+}
+
+
+export { person, car };
+export default printHobbies
---
+++ esmodules/esmodulesexample.js
@@ -0,0 +1,10 @@
+import { person } from "./esmodulesaula.js";
+import showHobby from "./esmodulesaula.js"
+import * as Everything from "./esmodulesaula.js"
+
+console.log(Everything.car)
+
+showHobby()
+
+console.log(person)
+
