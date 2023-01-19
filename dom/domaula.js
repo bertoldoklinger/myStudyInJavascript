ae1a1c2971112e515ef41f68c7f21248c9824084
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-01-19 15:55:35 -0300

    added dom aula + exercises foreach and math

---
+++ dom/domaula.js
@@ -0,0 +1,9 @@
+const title = document.getElementById("title");
+
+//console.log(title);
+
+const errors = document.getElementsByClassName("error");
+
+const paragraphs = document.getElementsByTagName("p");
+
+console.log(paragraphs);
