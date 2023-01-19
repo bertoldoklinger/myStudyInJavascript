4b89459f917b089a41b7068306476d5c2a23a17d
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-01-19 16:17:19 -0300

    added queryselector & queryselectorall methods study

--- dom/outrasqueries/outrasqueries.js
+++ dom/outrasqueries/outrasqueries.js
@@ -1,9 +1,8 @@
+// referenciando elementos por ID
 const title = document.getElementById("title");
 
-//console.log(title);
-
+//REFERENCIANDO ELEMENTOS PELA CLASSE CSS
 const errors = document.getElementsByClassName("error");
 
+//REFERENCIANDO ELEMENTOS NO DOM PELA TAG HTML
 const paragraphs = document.getElementsByTagName("p");
-
-console.log(paragraphs);
---
+++ dom/queryselector/app.js
@@ -0,0 +1,16 @@
+const errorDiv = document.querySelector("div.error");
+console.log(errorDiv);
+
+const h1 = document.querySelector("body > h1");
+
+console.log(h1);
+
+const paragraphs = document.querySelectorAll("p");
+
+paragraphs.forEach((paragraph) => {
+  console.log(paragraph);
+});
+
+const errors = document.querySelectorAll(".error");
+
+console.log(errors);
