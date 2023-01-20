969d8bd33495abfa75e8dd863346ca391b0e80b3
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-01-20 14:28:23 -0300

    finished obtaining and setting atribbutes on js(dom)

--- dom/ObtendoSetandoAtributos/app.js
+++ dom/ObtendoSetandoAtributos/app.js
@@ -10,3 +10,13 @@ console.log("🚀 ~ file: app.js:8 ~ paragraph", paragraph.getAttribute("class")
 paragraph.setAttribute("class", "success");
 
 paragraph.setAttribute("style", "color:green;");
+
+const h2 = document.querySelector("h2");
+// SETANDO ESTILOS PARA UM ELEMENTO HTML POR MEIO DO JAVASCRIPT
+h2.style.margin = "50px";
+h2.style.color = "red";
+h2.style.display = "flex";
+h2.style.justifyContent = "center";
+h2.style.fontSize = "40px";
+//REMOVENDO ESTILOS POR MEIO DO JAVASCRIPT
+h2.style.margin = "";
