db902b5492403b48bf94a52d8f5a57e2932b7b75
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-01-23 09:14:27 -0300

    adding event listeners on elements

---
+++ dom/event handling/app.js
@@ -0,0 +1,15 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  console.log("Clicou no botão");
+});
+
+const lis = document.querySelectorAll("li");
+
+lis.forEach((li) => {
+  li.addEventListener("click", (event) => {
+    const clickedElement = event.target;
+
+    clickedElement.style.textDecoration = "line-through";
+  });
+});
