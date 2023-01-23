912a8bdd0ce51fc461767aef96796eaf1da1c4bc
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-01-23 09:23:46 -0300

    added simple todo list exercises and learned event listeners

--- dom/event handling/app.js
+++ dom/event handling/app.js
@@ -1,8 +1,6 @@
-const button = document.querySelector("button");
+const ul = document.querySelector("ul");
 
-button.addEventListener("click", () => {
-  console.log("Clicou no botão");
-});
+//ul.remove();
 
 const lis = document.querySelectorAll("li");
 
@@ -10,6 +8,16 @@ lis.forEach((li) => {
   li.addEventListener("click", (event) => {
     const clickedElement = event.target;
 
-    clickedElement.style.textDecoration = "line-through";
+    clickedElement.remove();
   });
+});
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  const li = document.createElement("li");
+
+  li.textContent = "Novo Item";
+
+  ul.prepend(li);
 });
