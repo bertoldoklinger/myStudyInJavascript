93e55e8136b0836651e4986b74ef86e14c895cf7
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-01-26 15:32:13 -0300

    added keyboard validation on form and class change to success and error

--- eventhandling/formvalidation/app.js
+++ eventhandling/formvalidation/app.js
@@ -1,13 +1,13 @@
 const form = document.querySelector(".signup-form");
 const feedback = document.querySelector(".feedback");
 
+const testUsername = (username) => /^[a-zA-Z]{6,12}$/.test(username);
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const username = event.target.username.value;
 
-  const usernameRegex = /^[a-zA-Z]{6,12}$/;
-  const isAValidUsername = usernameRegex.test(username);
+  const isAValidUsername = testUsername(username);
 
   if (isAValidUsername) {
     feedback.textContent = "username válido =)";
@@ -15,4 +15,15 @@ form.addEventListener("submit", (event) => {
   }
   feedback.textContent =
     "O username deve conter entre 6 a 12 caracteres e deve conter apenas letras";
+});
+
+form.username.addEventListener("keyup", (event) => {
+  const isAValidUsername = testUsername(event.target.value);
+
+  if (isAValidUsername) {
+    form.username.setAttribute("class", "success");
+    return;
+  }
+
+  form.username.setAttribute("class", "error");
 });
