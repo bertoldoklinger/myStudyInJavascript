701e9e93b70f042177aeea33d644ecf7a1aacfa9
bertoldoklinger <bertoldokbrj@gmail.com>
2023-01-29 23:24:50 -0300

    added new codewars exercise odd or even

---
+++ codewarsexercises/oddoreven.js
@@ -0,0 +1,23 @@
+// Task:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+
+function oddOrEven(array) {
+  if (array === []) {
+    return "even";
+  }
+  return array.reduce((acc, item) => acc + item, 0) % 2 === 0 ? "even" : "odd";
+}
