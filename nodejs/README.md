e71dbde2377842807973a09a0f2cc624115ab347
bertoldoklinger <bertoldokbrj@gmail.com>
2023-02-05 18:53:57 -0300

    feat: user should be able to create and fetch a statement from his account

--- nodejs/README.md
+++ nodejs/README.md
@@ -4,8 +4,8 @@
 
 ### Requisitos
 
-- [] Deve ser possível criar uma conta
-- [] Deve ser possível buscar o extrato bancário do cliente
+- [x] Deve ser possível criar uma conta
+- [x] Deve ser possível buscar o extrato bancário do cliente
 - [] Deve ser possível realizar um depósito
 - [] Deve ser possível realizar um saque
 - [] Deve ser possível buscar o extrato bancário do cliente por data
@@ -18,8 +18,8 @@
 
 ### Regras de negócio
 
-- [] Não deve ser possível cadastrar uma conta com CPF já exístente
-- [] Não deve ser possível buscar extrato em uma conta não exístente
+- [x] Não deve ser possível cadastrar uma conta com CPF já exístente
+- [x] Não deve ser possível buscar extrato em uma conta não exístente
 - [] Não deve ser possível fazer depósito em uma conta não exístente
 - [] Não deve ser possível fazer saque em uma conta não exístente
 - [] Não deve ser possível excluir uma conta não exístente
--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -1,7 +1,25 @@
 const express = require("express");
-const { v4 } = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+function verifyIfExistssAccountCpf(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found" });
+  }
+
+  req.customer = customer;
+
+  return next();
+}
 
 /**
  * CPF - STRING
@@ -12,6 +30,30 @@ const app = express();
 
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({ error: "Customer already exists!" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return res.status(201).send();
+});
+
+// app.use(verifyIfExistssAccountCpf)
+
+app.get("/statement/", verifyIfExistssAccountCpf, (req, res) => {
+  const { customer } = req;
+  return res.json(customer.statement);
 });
 
 app.listen(3333);
