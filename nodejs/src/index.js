7a8239898d5282c083c2981a507baad7fbac191a
bertoldoklinger <bertoldokbrj@gmail.com>
2023-02-05 18:09:44 -0300

    installed uuid dependency

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const { v4 } = require("uuid");
 
 const app = express();
+
+/**
+ * CPF - STRING
+ * NAME - STRING
+ * ID - UUID(IDENTIFICADOR ÚNICO UNIVERSAL)
+ * STATEMENT(EXTRATO/LANÇAMENTOS) - []
+ */
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+});
 
 app.listen(3333);
