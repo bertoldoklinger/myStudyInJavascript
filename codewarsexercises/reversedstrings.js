9a91d0aba73be1f5d54b1edfd3364f901ffa00e0
bertoldoklinger <bertoldokbrj@gmail.com>
2023-02-15 22:04:43 -0300

    add new code wars exercise

---
+++ codewarsexercises/reversedstrings.js
@@ -0,0 +1,11 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str) {
+  return str
+    .split("")
+    .reverse()
+    .join("");
+}
