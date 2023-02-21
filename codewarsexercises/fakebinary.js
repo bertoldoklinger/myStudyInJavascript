a2ea37081fe905dbf3af1b5c957b134f0b59181d
bertoldoklinger <bertoldokbrj@gmail.com>
2023-02-20 22:29:35 -0300

    add new codewars exercise

---
+++ codewarsexercises/fakebinary.js
@@ -0,0 +1,9 @@
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+
+
+function fakeBin(x) {
+  return x.split('').map(n => n < 5 ? 0 : 1).join('');
+}
